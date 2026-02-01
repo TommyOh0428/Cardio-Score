9477795e214ed7fb056b10cddda0ba3f9aff4465
Tommy Oh <koa18@sfu.ca>
2026-01-31 22:51:15 -0800

    upload large dataset

--- src/predict.py
+++ src/predict.py
@@ -1,10 +1,22 @@
 from src.train_xgb import train_xgboost
+from src.train_lr import train_logistic_regression
 from src.utils import logger
 
 def predict():
     # logging config is now handled in utils
     logger.info("Prediction started.")
+    
+    # Train both models
+    logger.info("\n" + "="*60)
+    logger.info("TRAINING LOGISTIC REGRESSION")
+    logger.info("="*60)
+    train_logistic_regression()
+    
+    logger.info("\n" + "="*60)
+    logger.info("TRAINING XGBOOST")
+    logger.info("="*60)
     train_xgboost()
+    
     logger.info("Prediction finished.")
 
 
